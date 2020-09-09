6f86298de7c9f46ed6f9276fddd418817e963f39
DarkWeird <foxflameless@gmail.com>
2020-09-09 17:06:44 +0300

    feature(reintegrate-gestaltv7):  gestalt-v7 compability (package)

--- src/main/java/org/terasology/cab/generator/NativeTinGeneration.java
+++ src/main/java/org/terasology/cab/generator/NativeTinGeneration.java
@@ -1,33 +1,22 @@
-/*
- * Copyright 2016 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.cab.generator;
 
 import org.terasology.anotherWorld.decorator.ore.OreDefinition;
 import org.terasology.anotherWorld.decorator.structure.PocketStructureDefinition;
 import org.terasology.anotherWorld.decorator.structure.provider.UniformPocketBlockProvider;
 import org.terasology.anotherWorld.util.PDist;
-import org.terasology.registry.CoreRegistry;
-import org.terasology.world.block.BlockManager;
-import org.terasology.world.generator.plugin.RegisterPlugin;
+import org.terasology.engine.registry.CoreRegistry;
+import org.terasology.engine.world.block.BlockManager;
+import org.terasology.engine.world.generator.plugin.RegisterPlugin;
 
 @RegisterPlugin
 public class NativeTinGeneration extends PocketStructureDefinition implements OreDefinition {
     public NativeTinGeneration() {
-        super(new UniformPocketBlockProvider(CoreRegistry.get(BlockManager.class).getBlock("CopperAndBronze:NativeTin")),
-                new PDist(0.2f, 0.08f), new PDist(3f, 1f), new PDist(2f, 0.5f), new PDist(1700f, 200f), new PDist(0f, 0.35f),
+        super(new UniformPocketBlockProvider(CoreRegistry.get(BlockManager.class).getBlock("CopperAndBronze:NativeTin"
+                )),
+                new PDist(0.2f, 0.08f), new PDist(3f, 1f), new PDist(2f, 0.5f), new PDist(1700f, 200f), new PDist(0f,
+                        0.35f),
                 new PDist(1f, 0f), new PDist(0.7f, 0.1f), new PDist(0.2f, 0f), new PDist(0f, 0f));
     }
 
--- src/main/java/org/terasology/cab/system/CopperAndBronzeJournalIntegration.java
+++ src/main/java/org/terasology/cab/system/CopperAndBronzeJournalIntegration.java
@@ -2,67 +2,73 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.cab.system;
 
-import org.terasology.utilities.Assets;
-import org.terasology.workstationCrafting.component.CraftingStationIngredientComponent;
-import org.terasology.entitySystem.entity.EntityRef;
-import org.terasology.entitySystem.event.ReceiveEvent;
-import org.terasology.entitySystem.systems.BaseComponentSystem;
-import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.engine.entitySystem.entity.EntityRef;
+import org.terasology.engine.entitySystem.event.ReceiveEvent;
+import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
+import org.terasology.engine.entitySystem.systems.RegisterSystem;
+import org.terasology.engine.logic.characters.CharacterComponent;
+import org.terasology.engine.registry.In;
+import org.terasology.engine.rendering.nui.widgets.browser.data.ParagraphData;
+import org.terasology.engine.rendering.nui.widgets.browser.data.basic.HTMLLikeParser;
+import org.terasology.engine.rendering.nui.widgets.browser.ui.style.ParagraphRenderStyle;
+import org.terasology.engine.utilities.Assets;
+import org.terasology.inventory.logic.events.InventorySlotChangedEvent;
 import org.terasology.journal.BrowserJournalChapterHandler;
 import org.terasology.journal.DiscoveredNewJournalEntry;
 import org.terasology.journal.JournalEntryProducer;
 import org.terasology.journal.JournalManager;
 import org.terasology.journal.TimestampResolver;
-import org.terasology.logic.characters.CharacterComponent;
-import org.terasology.logic.inventory.events.InventorySlotChangedEvent;
-import org.terasology.registry.In;
 import org.terasology.nui.HorizontalAlign;
-import org.terasology.rendering.nui.widgets.browser.data.ParagraphData;
-import org.terasology.rendering.nui.widgets.browser.data.basic.HTMLLikeParser;
-import org.terasology.rendering.nui.widgets.browser.ui.style.ParagraphRenderStyle;
+import org.terasology.workstationCrafting.component.CraftingStationIngredientComponent;
 
 import java.util.Arrays;
 import java.util.Collection;
 
 @RegisterSystem
 public class CopperAndBronzeJournalIntegration extends BaseComponentSystem {
-    @In
-    private JournalManager journalManager;
-
-    private String chapterId = "CopperAndBronze";
-
-    private ParagraphRenderStyle centerRenderStyle = new ParagraphRenderStyle() {
+    private final String chapterId = "CopperAndBronze";
+    private final ParagraphRenderStyle centerRenderStyle = new ParagraphRenderStyle() {
         @Override
         public HorizontalAlign getHorizontalAlignment() {
             return HorizontalAlign.CENTER;
         }
     };
+    @In
+    private JournalManager journalManager;
 
     @Override
     public void initialise() {
         BrowserJournalChapterHandler chapterHandler = new BrowserJournalChapterHandler();
 
         chapterHandler.registerJournalEntry("chalcopyriteCrystalAndStation",
-                createTimestampEntryProducer("I found Chalcopyrite Crystal, it is a great source of copper. I should be able crush it in metal station into " +
-                        "Chalcopyrite Crystal Dust. Once in that form, I will be able to extract copper out of it to build stronger " +
-                        "tools.<l><l>To build the station, I need two Cobblestone blocks side-by-side and use my hammer on them " +
+                createTimestampEntryProducer("I found Chalcopyrite Crystal, it is a great source of copper. I should " +
+                        "be able crush it in metal station into " +
+                        "Chalcopyrite Crystal Dust. Once in that form, I will be able to extract copper out of it to " +
+                        "build stronger " +
+                        "tools.<l><l>To build the station, I need two Cobblestone blocks side-by-side and use my " +
+                        "hammer on them " +
                         "as with any previous stations."));
 
         chapterHandler.registerJournalEntry("chalcopyriteCrystal",
-                createTimestampEntryProducer("I found Chalcopyrite Crystal, it is a great source of copper. I should be able crush it in metal station into " +
-                        "Chalcopyrite Crystal Dust. Once in that form, I will be able to extract copper out of it to build stronger " +
+                createTimestampEntryProducer("I found Chalcopyrite Crystal, it is a great source of copper. I should " +
+                        "be able crush it in metal station into " +
+                        "Chalcopyrite Crystal Dust. Once in that form, I will be able to extract copper out of it to " +
+                        "build stronger " +
                         "tools."));
 
         chapterHandler.registerJournalEntry("nativeCopperAndStation",
-                createTimestampEntryProducer("I found Native Copper. Boy, am I lucky! This is a very rare find and is a pure source of copper. I should be able " +
+                createTimestampEntryProducer("I found Native Copper. Boy, am I lucky! This is a very rare find and is" +
+                        " a pure source of copper. I should be able " +
                         "to build stronger tools using it in metal station. To build the station, I need two " +
                         "Cobblestone blocks side-by-side and use my hammer on them as with any previous stations."));
 
         chapterHandler.registerJournalEntry("nativeCopper",
-                createTimestampEntryProducer("I found Native Copper. Boy, am I lucky! This is a very rare find and is a pure source of copper. I should be able " +
+                createTimestampEntryProducer("I found Native Copper. Boy, am I lucky! This is a very rare find and is" +
+                        " a pure source of copper. I should be able " +
                         "to build stronger tools using it in metal station."));
 
-        journalManager.registerJournalChapter(chapterId, Assets.getTexture("CopperAndBronze:CopperAndBronzeJournal").get(), "Copper and Bronze",
+        journalManager.registerJournalChapter(chapterId,
+                Assets.getTexture("CopperAndBronze:CopperAndBronzeJournal").get(), "Copper and Bronze",
                 chapterHandler);
     }
 
@@ -71,7 +77,8 @@ public class CopperAndBronzeJournalIntegration extends BaseComponentSystem {
             @Override
             public Collection<ParagraphData> produceParagraph(long date) {
                 return Arrays.asList(
-                        HTMLLikeParser.parseHTMLLikeParagraph(centerRenderStyle, TimestampResolver.getJournalEntryDate(date)),
+                        HTMLLikeParser.parseHTMLLikeParagraph(centerRenderStyle,
+                                TimestampResolver.getJournalEntryDate(date)),
                         HTMLLikeParser.parseHTMLLikeParagraph(null,
                                 text));
             }
@@ -79,13 +86,15 @@ public class CopperAndBronzeJournalIntegration extends BaseComponentSystem {
     }
 
     @ReceiveEvent
-    public void playerPickedUpItem(InventorySlotChangedEvent event, EntityRef character, CharacterComponent characterComponent) {
-        CraftingStationIngredientComponent ingredientComponent = event.getNewItem().getComponent(CraftingStationIngredientComponent.class);
+    public void playerPickedUpItem(InventorySlotChangedEvent event, EntityRef character,
+                                   CharacterComponent characterComponent) {
+        CraftingStationIngredientComponent ingredientComponent =
+                event.getNewItem().getComponent(CraftingStationIngredientComponent.class);
         if (ingredientComponent != null) {
             String ingredientType = ingredientComponent.type;
             if (ingredientType.equals("CopperAndBronze:chalcopyriteCrystal")
                     && !(journalManager.hasEntry(character, chapterId, "chalcopyriteCrystalAndStation")
-                            || journalManager.hasEntry(character, chapterId, "chalcopyriteCrystal"))) {
+                    || journalManager.hasEntry(character, chapterId, "chalcopyriteCrystal"))) {
                 if (journalManager.hasEntry(character, chapterId, "nativeCopperAndStation")) {
                     character.send(new DiscoveredNewJournalEntry(chapterId, "chalcopyriteCrystal"));
                 } else {
@@ -93,7 +102,7 @@ public class CopperAndBronzeJournalIntegration extends BaseComponentSystem {
                 }
             } else if (ingredientType.equals("CopperAndBronze:copperNugget")
                     && !(journalManager.hasEntry(character, chapterId, "nativeCopperAndStation")
-                            || journalManager.hasEntry(character, chapterId, "nativeCopper"))) {
+                    || journalManager.hasEntry(character, chapterId, "nativeCopper"))) {
                 if (journalManager.hasEntry(character, chapterId, "chalcopyriteCrystalAndStation")) {
                     character.send(new DiscoveredNewJournalEntry(chapterId, "nativeCopper"));
                 } else {
