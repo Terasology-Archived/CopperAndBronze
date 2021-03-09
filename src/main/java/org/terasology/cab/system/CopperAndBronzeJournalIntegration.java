3366eec1279e2b4f3c37fb1a7372f2a8abb9f963
Kevin Turner <83819+keturn@users.noreply.github.com>
2021-03-08 19:05:44 -0800

    chore: update engine package paths for https://github.com/MovingBlocks/Terasology/pull/4560

--- src/main/java/org/terasology/cab/system/CopperAndBronzeJournalIntegration.java
+++ src/main/java/org/terasology/cab/system/CopperAndBronzeJournalIntegration.java
@@ -2,24 +2,24 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.cab.system;
 
-import org.terasology.utilities.Assets;
-import org.terasology.workstationCrafting.component.CraftingStationIngredientComponent;
-import org.terasology.entitySystem.entity.EntityRef;
-import org.terasology.entitySystem.event.ReceiveEvent;
-import org.terasology.entitySystem.systems.BaseComponentSystem;
-import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.engine.entitySystem.entity.EntityRef;
+import org.terasology.engine.entitySystem.event.ReceiveEvent;
+import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
+import org.terasology.engine.entitySystem.systems.RegisterSystem;
+import org.terasology.engine.logic.characters.CharacterComponent;
+import org.terasology.engine.logic.inventory.events.InventorySlotChangedEvent;
+import org.terasology.engine.registry.In;
+import org.terasology.engine.rendering.nui.widgets.browser.data.ParagraphData;
+import org.terasology.engine.rendering.nui.widgets.browser.data.basic.HTMLLikeParser;
+import org.terasology.engine.rendering.nui.widgets.browser.ui.style.ParagraphRenderStyle;
+import org.terasology.engine.utilities.Assets;
 import org.terasology.journal.BrowserJournalChapterHandler;
 import org.terasology.journal.DiscoveredNewJournalEntry;
 import org.terasology.journal.JournalEntryProducer;
 import org.terasology.journal.JournalManager;
 import org.terasology.journal.TimestampResolver;
-import org.terasology.logic.characters.CharacterComponent;
-import org.terasology.logic.inventory.events.InventorySlotChangedEvent;
-import org.terasology.registry.In;
 import org.terasology.nui.HorizontalAlign;
-import org.terasology.rendering.nui.widgets.browser.data.ParagraphData;
-import org.terasology.rendering.nui.widgets.browser.data.basic.HTMLLikeParser;
-import org.terasology.rendering.nui.widgets.browser.ui.style.ParagraphRenderStyle;
+import org.terasology.workstationCrafting.component.CraftingStationIngredientComponent;
 
 import java.util.Arrays;
 import java.util.Collection;
